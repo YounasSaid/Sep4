f3cb1f85e298b2457afb0e9bca67de41508d7a31
Mazen Sharaf <mazen.sharaf03@gmail.com>
2026-03-27 14:19:49 +0100

    reactproject

---
+++ Frontend/my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+// her skal vi skrive vores react kode, hermed funktioner osv..
+function App() {
+  const [count, setCount] = useState(0) 
+}
+
+export default App
