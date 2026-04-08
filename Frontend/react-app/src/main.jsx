2462dbaa8ad3dafa6b07988ebc6afdcb686897b4
Mazen Sharaf <mazen.sharaf03@gmail.com>
2026-04-08 11:25:29 +0200

    routes

--- Frontend/react-app/src/main.jsx
+++ Frontend/react-app/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider, createHashRouter } from "react-router-dom";
+import { createHashRouter } from "create-router";
 
-createRoot(document.getElementById('root')).render(
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  /*,
+  {
+    path: "/about",
+    element: <h1>About</h1>,
+  } */
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
