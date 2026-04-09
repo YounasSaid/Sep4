cf41677438552d8fc1df88d0b2130a88d379077d
Mazen Sharaf <mazen.sharaf03@gmail.com>
2026-04-09 02:04:23 +0200

    compose + navbar

--- Frontend/react-app/src/App.jsx
+++ Frontend/react-app/src/App.jsx
@@ -3,13 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Welcome to Vite + React</h1>
+      <Navbar />
     </>
   )
 }
