8b1db0f699cd8e74b6bf82003242e9b2f3b2866b
Mazen Sharaf <mazen.sharaf03@gmail.com>
2026-04-22 17:34:33 +0200

    filer til send data

--- Frontend/react-app/src/components/FetchFunctions.jsx
+++ Frontend/react-app/src/components/FetchFunctions.jsx
@@ -1,5 +1,11 @@
-import { useEffect, useState, createContext, useContext, Suspense } from 'react'
-import "./css/FetchFunktions.css"
+import {
+  useEffect,
+  useState,
+  createContext,
+  useContext,
+  Suspense,
+} from "react";
+import "./css/FetchFunktions.css";
 
 export default function SensorData() {
   const [temperature, setTemperature] = useState(null);
@@ -11,7 +17,7 @@ export default function SensorData() {
   const fetchData = async (type, setter) => {
     try {
       const res = await fetch(
-        `https://sep4-server.azurewebsites.net/api/measurement/latest?type=${type}` //henter seneste data
+        `https://sep4-server.azurewebsites.net/api/measurement/latest?type=${type}`, //henter seneste data
       );
       const data = await res.json();
       setter(data.value);
@@ -19,7 +25,7 @@ export default function SensorData() {
       console.log(error);
     }
   };
-    useEffect(() => {
+  useEffect(() => {
     fetchData("temperature", (value) => setTemperature(value.toFixed(2)));
     fetchData("soil_moisture", (value) => setSoilMoisture(value.toFixed(2)));
     fetchData("humidity", (value) => setHumidity(value.toFixed(2)));
@@ -27,8 +33,8 @@ export default function SensorData() {
     fetchData("height", (value) => setHeight(value.toFixed(2)));
   }, []);
 
-   return (
-    <div className='sensor-data'>
+  return (
+    <div className="sensor-data">
       <h1>Sensor Data:</h1>
       <p>Temperature: {temperature}°C</p>
       <p>Soil Moisture: {soilMoisture}%</p>
@@ -37,4 +43,26 @@ export default function SensorData() {
       <p>Height: {height} cm</p>
     </div>
   );
+}
+
+export async function SendData(type, value) {
+  try {
+    const response = await fetch(
+      "https://sep4-server.azurewebsites.net/api/measurement",
+      {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ type: type, value: value }),
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(`Server error: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
 }
---
+++ Frontend/react-app/src/components/SendData.jsx
@@ -0,0 +1,23 @@
+import {
+  useEffect,
+  useState,
+  createContext,
+  useContext,
+  Suspense,
+} from "react";
+import { SendData } from "./FetchFunctions";
+
+function App() {
+  const [type, setType] = useState("height");
+  const [value, setValue] = useState("");
+
+  };
+  return (
+    // her skal html kode være
+    <div></div>
+  );
+
+
+export function NewData() {
+  return <div id="NewData"></div>;
+}
