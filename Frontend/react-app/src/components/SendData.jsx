a2e4e6cf87a8deed95371ce80b863a154eaa114c
Mazen Sharaf <mazen.sharaf03@gmail.com>
2026-04-27 22:37:03 +0200

    saleeman's opgave, sender til den rigtige branch

--- Frontend/react-app/src/components/SendData.jsx
+++ Frontend/react-app/src/components/SendData.jsx
@@ -21,7 +21,20 @@ function SendDataComponent() {
   };
   return (
     // her skal html kode være
-    <div></div>
+     <div className="side">
+      <div className="indhold">
+        <h1 className="titel">Drivhusprojektet</h1>
+        <div className="input-boks">
+          <label>Indtast højde:</label>
+          <input type="number" placeholder="cm" />
+        </div>
+        <button className="send-knap">Send</button>
+      </div>
+      <div className="sidebar">
+        <a href="#">Mål Højde</a>
+        <a href="#">Sensor Grafer</a>
+      </div>
+    </div>
   );
 }
 
