63a9002ab11ca82fcda687affc45637b6b064e14
FlemsSolo <354019@via.dk>
2026-05-05 22:49:42 +0200

    feature/WEB/DatoTidFelter (test)

---
+++ Frontend/react-app/src/components/DateTimeInput.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState, createContext, useContext, Suspense } from 'react'
+import { HashRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+
+//import styles from '../App.module.css'
+
+export function DateTimeInput()
+  {
+  return (
+    <div>
+    <h1>Show a Date and Time Control</h1>
+    <form>
+        <label for="birthdaytime">Start :</label>
+        <input type="datetime-local" id="startdaytime" name="startdaytime" />
+
+        <label for="birthdaytime">Slut :</label>
+        <input type="datetime-local" id="slutdaytime" name="slutdaytime" />
+
+        <input type="submit" />
+    </form>
+    </div>
+    ) ;
+  }
