1e9243959bd0bfa2f1ced4646aea3956932e1511
Mazen Sharaf <mazen.sharaf03@gmail.com>
2026-05-08 10:07:18 +0200

    routing

--- Frontend/react-app/src/components/Routing.jsx
+++ Frontend/react-app/src/components/Routing.jsx
@@ -1,18 +1,31 @@
-import { useEffect, useState, createContext, useContext, Suspense } from 'react'
-import { BrowserRouter, HashRouter, Routes, Route, Link, NavLink } from "react-router-dom";
-import { Charts } from "./SensorChart.jsx"
-import SendHeight from './FetchFunctions.jsx';
-import { NewData } from './SendHeight.jsx';
-import { StartPage } from './StartPage.jsx';
-import { WaterControl } from './Watercontrol.jsx';
+import {
+  useEffect,
+  useState,
+  createContext,
+  useContext,
+  Suspense,
+} from "react";
+import {
+  BrowserRouter,
+  HashRouter,
+  Routes,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
+import { Charts } from "./SensorChart.jsx";
+import SendHeight from "./FetchFunctions.jsx";
+import { NewData } from "./SendHeight.jsx";
+import { StartPage } from "./StartPage.jsx";
+import { WaterControl } from "./Watercontrol.jsx";
 
 export function Routing() {
-
   return (
     <Routes>
       <Route path="/" element={<StartPage />} />
       <Route path="/SendHeight" element={<NewData />} />
       <Route path="/Charts" element={<Charts />} />
+      <Route path="/WaterControl" element={<WaterControl />} />
     </Routes>
   );
 }
