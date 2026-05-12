39dbc94e8ed0dcf3ffe19d0cb120138198a73d50
Malthe Laursen <malthe@mtsl.dk>
2026-05-12 23:04:28 +0200

    CORS hotfix for auth

--- MAL/Server/GreenHouseApi/Program.cs
+++ MAL/Server/GreenHouseApi/Program.cs
@@ -15,9 +15,16 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
+        policy.WithOrigins(
+                "http://182.20.128.1:5173", // Udvikler adresse
+                "https://182.20.128.1:5173", // Udvikler adresse
+                "http://localhost:5173", // Udvikler adresse
+                "https://localhost:5173", // Udvikler adresse
+                "http://4.223.137.178",
+                "https://4.223.137.178")
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials(); // KRÆVES for HttpOnly cookies
     });
 });
 
