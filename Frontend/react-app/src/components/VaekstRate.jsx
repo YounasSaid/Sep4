b281507ac9e3388ce12c7a354d0c394b5384a18b
Mazen Sharaf <mazen.sharaf03@gmail.com>
2026-05-16 13:54:04 +0200

    label&statusText

--- Frontend/react-app/src/components/VaekstRate.jsx
+++ Frontend/react-app/src/components/VaekstRate.jsx
@@ -18,6 +18,16 @@ function VaekstRate() {
   const [light, setLight] = useState(null);
   const [height, setHeight] = useState(null);
 
+  //status for data hentning
+  const [temp_min, setTemp_min] = useState(null); 
+  const [temp_max, setTemp_max] = useState(null);
+  const [hum_min, setHum_min] = useState(null);
+  const [hum_max, setHum_max] = useState(null);
+  const [soil_min, setSoil_min] = useState(null);
+  const [soil_max, setSoil_max] = useState(null);
+  const [light_min, setLight_min] = useState(null);
+  const [light_max, setLight_max] = useState(null);
+
   // Denne funktion henter data og sender til ML
   const hentOgForudsig = async (type, setter) => {
     try {
@@ -44,26 +54,43 @@ function VaekstRate() {
     hentOgForudsig("soil", (value) => setSoil(value / 1023));
     hentOgForudsig("height", (value) => setHeight(value));
 
+    hentOgForudsig("temp_min", (value) => setTemp_min(value));
+    hentOgForudsig("temp_max", (value) => setTemp_max(value));
+    hentOgForudsig("hum_min", (value) => setHum_min(value));
+    hentOgForudsig("hum_max", (value) => setHum_max(value));
+    hentOgForudsig("soil_min", (value) => setSoil_min(value / 1023));
+    hentOgForudsig("soil_max", (value) => setSoil_max(value / 1023));
+    hentOgForudsig("light_min", (value) => setLight_min(value / 1023));
+    hentOgForudsig("light_max", (value) => setLight_max(value / 1023));
+
+      
     // Gem tidspunkt for opdatering
     const nu = new Date();
     setSidstOpdateret(nu.toLocaleString("da-DK"));
   }, []);
+     // laver en funktion der tjekker om værdien er inden for det optimale interval og returnere en emoji
+     const getEmojiStatus = (type, value, min, max) => {
+     if (value < min || value > max) return "⚠️";
+     return "✅";
+    };
 
-  const erGod = 1; //resultat.growth_milestone === 1;
-  const procent = 75; //Math.round(resultat.probability.milestone_reached * 100);
-
+   const getTextStatus = (type, value, min, max) => {
+     if (value < min || value > max) return "Dårlige vækstforhold";
+     return "Gode vækstforhold";
+    }
+   
   return (
     <div className="vaekst-side">
       {/* Titel */}
       <h1 className="titel">🌱 VækstRate Forudsigelse</h1>
 
       {/* Grøn eller rød indikator */}
-      <div className={erGod ? "indikator groen" : "indikator roed"}>
-        <span>{erGod ? "✅" : "⚠️"}</span>
+      <div className={getEmojiStatus ? "indikator groen" : "indikator roed"}>
+        <span>{getEmojiStatus ? "✅" : "⚠️"}</span>
         <span>
-          {erGod
-            ? `Gode vækstforhold (${procent}%)`
-            : `Dårlige vækstforhold (${procent}%)`}
+          {getEmojiStatus
+            ? `Gode vækstforhold (${temp}%)`
+            : `Dårlige vækstforhold (${temp}%)`}
         </span>
       </div>
 
@@ -72,12 +99,12 @@ function VaekstRate() {
         <div
           className="progress-bar-fyld"
           style={{
-            width: `${procent}%`,
-            backgroundColor: erGod ? "#4caf50" : "#f44336",
+            width: `${temp}%`,
+            backgroundColor: getEmojiStatus ? "#4caf50" : "#f44336",
           }}
         />
       </div>
-      <p className="procent-tekst">{procent}%</p>
+      <p className="procent-tekst">{temp}%</p>
 
       {/* Sensor tabel */}
       <h2 className="sensor-titel">Baseret på seneste målinger:</h2>
@@ -85,36 +112,36 @@ function VaekstRate() {
         <div className="sensor-raekke">
           <span>🌡️ Temperatur</span>
           <span>{temp != null ? temp?.toFixed(2) + "°C" : "Ingen data!"}</span>
-          <span>{true || resultat?.temperature_ok ? "✅" : "⚠️"}</span>
-          <span>OK!({resultat?.temperature_optimal_range})</span>
+          <span>{getEmojiStatus("temp", temp?.toFixed(2), temp_min?.toFixed(2), temp_max?.toFixed(2))}</span>
+          <span>{getTextStatus("temp", temp?.toFixed(2), temp_min?.toFixed(2), temp_max?.toFixed(2))}</span>
         </div>
 
         <div className="sensor-raekke">
           <span>💨 Luftfugtighed</span>
           <span>{hum != null ? hum?.toFixed(2) + "%" : "Ingen data!"}</span>
-          <span>{false || resultat?.humidity_ok ? "✅" : "⚠️"}</span>
-          <span>Warning!({resultat?.humidity_optimal_range})</span>
+          <span>{getEmojiStatus("hum", hum?.toFixed(2), hum_min?.toFixed(2), hum_max?.toFixed(2))}</span>
+          <span>{getTextStatus("hum", hum?.toFixed(2), hum_min?.toFixed(2), hum_max?.toFixed(2))}</span>
         </div>
 
         <div className="sensor-raekke">
           <span>💧 jordfugtighed</span>
           <span>{soil != null ? soil?.toFixed(2) + "%" : "Ingen data!"}</span>
-          <span>{true || resultat?.soil_ok ? "✅" : "⚠️"}</span>
-          <span>(OK!{resultat?.soil_optimal_range})</span>
+          <span>{getEmojiStatus("soil", soil?.toFixed(2), soil_min?.toFixed(2), soil_max?.toFixed(2))}</span>
+          <span>{getTextStatus("soil", soil?.toFixed(2), soil_min?.toFixed(2), soil_max?.toFixed(2))}</span>
         </div>
 
         <div className="sensor-raekke">
           <span>☀️ Lys</span>
           <span>{light != null ? light?.toFixed(2) + "%" : "Ingen data!"}</span>
-          <span>{true || resultat?.light_ok ? "✅" : "⚠️"}</span>
-          <span>(OK!{resultat?.light_optimal_range})</span>
+          <span>{getEmojiStatus("light", light?.toFixed(2), light_min?.toFixed(2), light_max?.toFixed(2))}</span>
+          <span>{getTextStatus("light", light?.toFixed(2), light_min?.toFixed(2), light_max?.toFixed(2))}</span>
         </div>
 
         <div className="sensor-raekke">
           <span>📏 højde</span>
           <span>{height != null ? height?.toFixed(2) + " cm" : "Ingen data!"}</span>
-          <span>{true || resultat?.height_ok ? "✅" : "⚠️"}</span>
-          <span>(OK!{resultat?.height_optimal_range})</span>
+          <span> ! </span>
+          <span> ! </span>
         </div>
       </div>
 
